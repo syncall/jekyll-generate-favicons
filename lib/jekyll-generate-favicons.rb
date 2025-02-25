d63171ce65a86d72a46c95540ea19973fa1c5d30
Markus Sosnowski <sosnowski@net.in.tum.de>
2025-02-25 12:21:02 +0100

    default paht for apple touch icon

--- lib/jekyll-generate-favicons.rb
+++ lib/jekyll-generate-favicons.rb
@@ -6,48 +6,49 @@ require "jekyll-generate-favicons/utils"
 
 module Jekyll
   module GenerateFavicons
-      class FaviconTag < Liquid::Tag
+    class FaviconTag < Liquid::Tag
 
-        def initialize(tag_name, text, tokens)
-          super
+      DEFAULT_PATH = "assets/img/favicons".freeze
+
+      def render(context)
+        site = context.registers[:site]
+        baseurl = site.config["baseurl"]
+
+        icon_file = site.config["icon"]
+
+        apple_icon_file = site.config["apple_touch_icon"]
+
+        s = StringIO.new
+        if icon_file
+          icon_path, _ = Jekyll::GenerateFavicons::find_icon_file(site, icon_file)
+          s << "<link rel=\"icon\" type=\"image/x-icon\" sizes=\"48x48\" href=\"#{baseurl}/favicon.ico\">\n"
+          s << "<link rel=\"icon\" type=\"image/svg+xml\" sizes=\"any\" href=\"#{baseurl}/#{icon_path}\">\n"
         end
 
-        def render(context)
-          site = context.registers[:site]
-          baseurl = site.config["baseurl"]
+        if apple_icon_file
+          # apple_icon_file, _ = Jekyll::GenerateFavicons::find_icon_file(site, apple_icon_file)
+          default_path = Jekyll::GenerateFavicons::FaviconTag::DEFAULT_PATH
 
-          icon_file = site.config["icon"]
-
-          apple_icon_file = site.config["apple_touch_icon"]
-
-          s = StringIO.new
-          if icon_file
-            icon_path, _ = Jekyll::GenerateFavicons::find_icon_file(site, icon_file)
-            s << "<link rel=\"icon\" type=\"image/x-icon\" sizes=\"48x48\" href=\"#{baseurl}/favicon.ico\">\n"
-            s << "<link rel=\"icon\" type=\"image/svg+xml\" sizes=\"any\" href=\"#{baseurl}/#{icon_path}\">\n"
-          end
-
-          if apple_icon_file
-            # apple_icon_file, _ = Jekyll::GenerateFavicons::find_icon_file(site, apple_icon_file)
-
-            [120, 152, 167, 180].each { |size|
-              s << "<link rel=\"apple-touch-icon\" sizes=\"#{size}x#{size}\" href=\"#{site.baseurl}/assets/img/favicons/apple-touch-icon-#{size}.png\">\n"
-            }
-          end
-          apple_mask_icon = site.config["apple_mask_icon"]
-          if apple_mask_icon
-            mask_src = apple_mask_icon["src"]
-            mask_color = apple_mask_icon["color"]
-            mask_color = "black" unless mask_color
-
-            mask_src = apple_mask_icon unless mask_src
-
-            icon_path, _ = Jekyll::GenerateFavicons::find_icon_file(site, mask_src)
-            s << "<link rel=\"mask-icon\" href=\"#{baseurl}/#{icon_path}\" color=\"#{mask_color}\">"
-          end
-          s.string
+          [120, 152, 167, 180].each { |size|
+            icon_url = "#{site.baseurl}/#{default_path}/apple-touch-icon-#{size}.png"
+            icon_url = "#{site.baseurl}/apple-touch-icon.png" if size == 180
+            s << "<link rel=\"apple-touch-icon\" sizes=\"#{size}x#{size}\" href=\"#{icon_url}\">\n"
+          }
         end
+        apple_mask_icon = site.config["apple_mask_icon"]
+        if apple_mask_icon
+          mask_src = apple_mask_icon["src"]
+          mask_color = apple_mask_icon["color"]
+          mask_color = "black" unless mask_color
+
+          mask_src = apple_mask_icon unless mask_src
+
+          icon_path, _ = Jekyll::GenerateFavicons::find_icon_file(site, mask_src)
+          s << "<link rel=\"mask-icon\" href=\"#{baseurl}/#{icon_path}\" color=\"#{mask_color}\">"
+        end
+        s.string
       end
+    end
     end
 
 end
@@ -83,11 +84,15 @@ Jekyll::Hooks.register :site, :post_write do |site|
 
     _, src_file = Jekyll::GenerateFavicons::find_icon_file(site, apple_icon_file)
 
-    dst_dir = File.join(site.dest, "assets/img/favicons")
+    dst_default_file = File.join(site.dest, "apple-touch-icon.png")
+
+    dst_dir = File.join(site.dest, Jekyll::GenerateFavicons::FaviconTag::DEFAULT_PATH)
     FileUtils.mkdir_p dst_dir
 
     [120, 152, 167, 180].each { |size|
       dst_file = File.join(dst_dir, "apple-touch-icon-#{size}.png")
+      # Default is big
+      dst_file = dst_default_file if size == 180
       unless File.exist?(dst_file)
         cmd = "magick #{src_file.shellescape} -resize #{size}x#{size} #{dst_file.shellescape}"
         system(cmd)
